06c3ccbf5323277e4c194c22081c073f256ba2ff
dmills <dmills@iastate.edu>
2015-02-08 16:12:58 -0600

    updated readme and added group number to site

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php $active = 'home'; ?>
 <?php include_once 'src/php/parts/header.php'; ?>
 <div class='container'>
-  <h2> Homepage Stuff <i class='fa fa-smile-o'></i></h2>
+  <h2> Group Dec1512 <i class='fa fa-smile-o'></i></h2>
   <p> <a href='http://paninij.org/'> PaniniJ </a> </p>
 </div>
 <?php include_once 'src/php/parts/footer.php'; ?>
