20188e907c34d86b40f7eead134e9acd66bb4a10
dmills <dmills@iastate.edu>
2015-02-16 10:32:14 -0600

    added week 4, fixed css alignment

--- reports.php
+++ reports.php
@@ -2,10 +2,11 @@
 <?php include_once 'src/php/parts/header.php'; ?>
 <div class='container'>
   <ul>
-    <li><a href='/docs/report/week-0.pdf'>Week 0</a></li>
-    <li><a href='/docs/report/week-1.pdf'>Week 1</a></li>
-    <li><a href='/docs/report/week-2.pdf'>Week 2</a></li>
+    <li><a href='/docs/report/week-4.pdf'>Week 4</a></li>
     <li><a href='/docs/report/week-3.pdf'>Week 3</a></li>
+    <li><a href='/docs/report/week-2.pdf'>Week 2</a></li>
+    <li><a href='/docs/report/week-1.pdf'>Week 1</a></li>
+    <li><a href='/docs/report/week-0.pdf'>Week 0</a></li>
   </ul>
 </div>
 <?php include_once 'src/php/parts/footer.php'; ?>
