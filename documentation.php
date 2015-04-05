b430513bd1e256afe46301d6a9694ee767824164
dmills <dmills@iastate.edu>
2015-04-04 19:08:02 -0500

    added version 2 of project plan

--- documentation.php
+++ documentation.php
@@ -3,7 +3,8 @@
 <div class='container'>
   <h2><i class='fa fa-fw fa-file-o'></i> Documentation Stuffs </h2>
   <ul>
-    <li><a href='/docs/project/plan-1.pdf'>Project Plan - 1<sup>st</sup> Iteration</a></li>
+    <li><a href='/docs/project/plan-2.pdf'>Project Plan - Iteration 2 <em>*new*</em></a></li>
+    <li><a href='/docs/project/plan-1.pdf'>Project Plan - Iteration 1</a></li>
     <li><a href='/docs/project/design-preliminary.pdf'>Project Design - Preliminary</a></li>
   </ul>
 </div>
