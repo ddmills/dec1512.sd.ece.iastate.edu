5ce15269c0a81e6ba6d3e2a7a0bd44ec5de7ce99
dmills <dmills@iastate.edu>
2015-04-25 21:34:05 -0500

    added new version of design document

--- documentation.php
+++ documentation.php
@@ -5,6 +5,7 @@
   <li><a href='/docs/project/plan-3.pdf'>Project Plan - Iteration 3 <em>*new*</em></a></li>
   <li><a href='/docs/project/plan-2.pdf'>Project Plan - Iteration 2</a></li>
   <li><a href='/docs/project/plan-1.pdf'>Project Plan - Iteration 1</a></li>
-  <li><a href='/docs/project/design-preliminary.pdf'>Project Design - Preliminary</a></li>
+  <li><a href='/docs/project/design-2.pdf'>Project Design</a></li>
+
 </ul>
 <?php include_once 'src/php/parts/footer.php'; ?>
