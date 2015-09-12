ddc1d9405196f21468066551428eb5f1b478a1f6
ddmills <dmills@iastate.edu>
2015-09-12 15:44:53 -0500

    added semester-2 week-3 report, organized reports by semester

--- www/reports.php
+++ www/reports.php
@@ -4,23 +4,24 @@
 <div class='container'>
   <h3> Semester 2 <small>(Fall)</small> </h3>
   <ul>
-    <li><a href='/docs/report/week-13.pdf'>Week 13</a></li>
+    <li><a href='/docs/report/sem-2/week-3.pdf'>Week 3</a></li>
+    <li><a href='/docs/report/sem-2/week-1.pdf'>Week 1</a></li>
   </ul>
 
   <h3> Semester 1 <small>(Spring)</small> </h3>
   <ul>
-    <li><a href='/docs/report/week-12.pdf'>Week 12</a></li>
-    <li><a href='/docs/report/week-10-11.pdf'>Week 10 &amp; 11</a></li>
-    <li><a href='/docs/report/week-9.pdf'>Week 9</a></li>
-    <li><a href='/docs/report/week-8.pdf'>Week 8</a></li>
-    <li><a href='/docs/report/week-7.pdf'>Week 7</a></li>
-    <li><a href='/docs/report/week-6.pdf'>Week 6</a></li>
-    <li><a href='/docs/report/week-5.pdf'>Week 5</a></li>
-    <li><a href='/docs/report/week-4.pdf'>Week 4</a></li>
-    <li><a href='/docs/report/week-3.pdf'>Week 3</a></li>
-    <li><a href='/docs/report/week-2.pdf'>Week 2</a></li>
-    <li><a href='/docs/report/week-1.pdf'>Week 1</a></li>
-    <li><a href='/docs/report/week-0.pdf'>Week 0</a></li>
+    <li><a href='/docs/report/sem-1/week-12.pdf'>Week 12</a></li>
+    <li><a href='/docs/report/sem-1/week-10-11.pdf'>Week 10 &amp; 11</a></li>
+    <li><a href='/docs/report/sem-1/week-9.pdf'>Week 9</a></li>
+    <li><a href='/docs/report/sem-1/week-8.pdf'>Week 8</a></li>
+    <li><a href='/docs/report/sem-1/week-7.pdf'>Week 7</a></li>
+    <li><a href='/docs/report/sem-1/week-6.pdf'>Week 6</a></li>
+    <li><a href='/docs/report/sem-1/week-5.pdf'>Week 5</a></li>
+    <li><a href='/docs/report/sem-1/week-4.pdf'>Week 4</a></li>
+    <li><a href='/docs/report/sem-1/week-3.pdf'>Week 3</a></li>
+    <li><a href='/docs/report/sem-1/week-2.pdf'>Week 2</a></li>
+    <li><a href='/docs/report/sem-1/week-1.pdf'>Week 1</a></li>
+    <li><a href='/docs/report/sem-1/week-0.pdf'>Week 0</a></li>
   </ul>
 </div>
 <?php include_once './components/footer.php'; ?>
