8b4f0c69c41857fdcf0dc6eac65ca9a62edde050
ddmills <dmills@iastate.edu>
2015-12-08 23:47:09 -0600

    added final report

--- www/documentation.php
+++ www/documentation.php
@@ -2,10 +2,10 @@
 <?php include_once './components/header.php'; ?>
 <h2><i class='fa fa-fw fa-file-o'></i> Documentation </h2>
 <ul>
-  <li><a href='/docs/project/plan-3.pdf'>Project Plan - Iteration 3 <em>*new*</em></a></li>
+  <li><a href='/docs/project/final-report.pdf'>Final Report <em>* new *</em></a></li>
+  <li><a href='/docs/project/plan-3.pdf'>Project Plan - Iteration 3</a></li>
   <li><a href='/docs/project/plan-2.pdf'>Project Plan - Iteration 2</a></li>
   <li><a href='/docs/project/plan-1.pdf'>Project Plan - Iteration 1</a></li>
   <li><a href='/docs/project/design-2.pdf'>Project Design</a></li>
-
 </ul>
 <?php include_once './components/footer.php'; ?>
